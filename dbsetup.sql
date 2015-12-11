5ede96d98f523ae545febebb2f5791a1295a5dec
= <=>
2015-12-11 18:03:28 +0000

    Added dbsetup.sql to the repo

---
+++ dbsetup.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE tcount;
+/*Connect to database*/
+\c tcount;
+
+CREATE TABLE table_name (
+    column_name1 col_type (field_length) column_constraints,
+    column_name2 col_type (field_length),
+    );
+
